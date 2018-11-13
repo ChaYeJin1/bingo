875202af9f8e1ae8305d2b4a2b4f9e3171afb831
ChaYeJin1 <cha9599831@daum.net>
2018-11-14 00:02:43 +0900

    bingo_02

--- bingo_1.c
+++ bingo_1.c
@@ -2,18 +2,18 @@
 #include <stdlib.h>
 
 #define MAX_NUMBER N
-#define SIZE N
+#define SIZE N*N
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
 void initiate_bingo(int *arr);
 void print_bingo(int arr[N][N]);
 void process_bingo(int arr[N][N]);
-void change_bingo_table(int arr[N][N], int number);
+void check_bingo(int arr[N][N], int number);
 void print_winner(int winner);
 int get_number_byMe(int number);
 int get_number_byCom(int number);
-int count_bingo();
+int count_bingo(int [N][N], int num);
 int count = 0;
 int bingo_byMe(int arr[N][N]);
 int bingo_byCom(int arr[N][N]);
@@ -41,20 +41,44 @@ void print_bingo(int arr[N][N]){
 	}
 }
 	
-int get_number_by(int who){
+int get_number_byMe(int number){
 	int number;
-	 
-	do{
-		if(who == 1){
+	int x;
+	
+    do{
 			printf("N의 제곱까지의 숫자를 입력하시오 : ");
 			scanf("%d", &number); 
-		}
-		else{
-			number = rand() % N + 1;
-		}	
-}while();
+			 if(checked[x] == number){
+			  printf("이미 입력한 숫자입니다. 다시 입력하시오 : ");
+			  scanf("%d", &number);
+			}
+	}while(number<N*N);
+	
+	printf("사용자가 %d를 선택하였습니다 : \n", number);
+	
+	return number;
+}
 
-void change_bingo_table(int arr[N][N], int input_number){
+int get_number_byCom(int number){
+	int number;
+	int x;
+	int try_again;
+	
+	do{
+	    number = rand() % N*N + 1;
+		if(checked[x] == number){
+			try_again = 0; //try_again=0이면 다시 시도 
+			break;
+		} 
+	}while(try_again == 0);
+	
+	printf("컴퓨터가 %d를 선택하였습니다 : \n", number);
+	
+	return number;
+}
+	
+		
+void check_bingo(int arr[N][N], int input_number){
 	int x,y;
 	
 	for(x=0; x<SIZE; x++){
@@ -80,9 +104,45 @@ void print_winner(int winner){
 	}
 }
 
+int count_bingo(int [N][N], int num){
+	int bingo = 0;
+	int x, y;
+	int horizontal_line, vertical_line, diagonal_line_leftside, diagonal_line_rightside;
+	
+	for(x=0; x<num; x++){
+		horizontal_line = 0;
+		for(y=0; y<num; y++){
+			if(arr[N][N] == )
+			horizontal_line++;
+			
+			if(){
+				if(arr[N][N] == )
+				diagonal_line_leftside++;
+			}
+			
+			if(){
+				if(arr[N][N] == )
+				diagonal_line_rightside++;
+			}
+		}
+		if(horizontal_line == M){
+			bingo++;
+		}
+	}  
+	
+	for(y=0; y<num; y++){
+		vertical_line = 0;
+		for(x=0; x<num; x++){
+			if(arr[N][N] == )
+			vertical_line++;
+		}
+		if(vertical_line == M){
+			bingo++;
+		}
+	}
+}
+
 	
 
-
-	}
 	return 0;
 }
